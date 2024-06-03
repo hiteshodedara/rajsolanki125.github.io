61c374488d0a9753856a109309dc863dabff8cc1
rajsolanki125 <raj6solanki@gmail.com>
2024-06-03 12:56:06 +0530

    uupdate

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $('#myForm').on('submit', function(e){
-	// $('#myModal').modal('show');
+	$('#myModal').modal('show');
 	$('#sendmessage').css("display", "block");
-	
+
 	e.preventDefault();
   });
 
